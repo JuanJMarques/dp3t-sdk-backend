148f0f228853adcbcf520b82c27eabfe6b8ef31f
Patrick Amrein <amrein@ubique.ch>
2020-07-29 13:42:49 +0200

    Update to new version of swagger-gen use new psots and model

---
+++ dpppt-backend-sdk/dpppt-backend-sdk-model/src/main/java/org/dpppt/backend/sdk/model/gaen/GaenV2Request.java
@@ -0,0 +1,33 @@
+package org.dpppt.backend.sdk.model.gaen;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import ch.ubique.openapi.docannotations.Documentation;
+
+public class GaenV2Request {
+    @NotNull
+    @NotEmpty
+    @Valid
+    @Documentation(description = "A list of iso-3166-1 alpha-2 country codes", example = "[\"CH\", \"DE\"]")
+    List<String> countryCodes;
+
+    @NotNull
+    @NotEmpty
+    @Valid
+    @Size(min = 30, max = 30)
+    @Documentation(description = "30 Temporary Exposure Keys - zero or more of them might be fake keys.")
+    private List<GaenKey> gaenKeys;
+
+    public List<GaenKey> getGaenKeys() {
+        return this.gaenKeys;
+    }
+
+    public void setGaenKeys(List<GaenKey> gaenKeys) {
+        this.gaenKeys = gaenKeys;
+    }
+}
--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/controller/GaenV2Controller.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/controller/GaenV2Controller.java
@@ -1,9 +1,12 @@
 package org.dpppt.backend.sdk.ws.controller;
 
+import org.dpppt.backend.sdk.model.gaen.GaenV2Request;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,6 +18,13 @@ import ch.ubique.openapi.docannotations.Documentation;
 public class GaenV2Controller {
 
 	/// POST für Keyupload
+	@PostMapping(path = "/exposed")
+	@Documentation(description = "Endpoint used to upload exposure keys to the backend specifying for which countries the keys are valid.")
+	public @ResponseBody ResponseEntity<String> addExposed(
+			@Documentation(description = "JSON Object containing all keys and a list of countries specifying for which countries the keys are valid") @RequestBody GaenV2Request gaenV2Request) {
+
+		return null;
+	}
 
 	/// GET fuer Key Download
 	@GetMapping(path = "/exposed/{country}/{since}")
